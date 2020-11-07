b3231f42272539b21264e820b277c1619b1420ee
emicasasola <emi_22792@hotmail.com>
2020-11-06 19:21:43 -0600

    lastCommit

--- api/restassured/src/test/java/Tests.java
+++ api/restassured/src/test/java/Tests.java
@@ -1,3 +1,4 @@
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.testng.annotations.Test;
 
 
@@ -5,13 +6,16 @@ import static org.hamcrest.core.IsEqual.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
 
 public class Tests extends BaseTest{
 
     private static final String ResourceGetStatus= "/v1/examen/status";
     private static final String ResourcePutName= "/v1/examen/updateName";
     private static final String ResourceGetRandomName= "/v1/examen/randomName";
-    private static final String ResourcePostSameName= "/v1/examen/randomName";
+    private static final String ResourcePostSameName= "/v1/examen/sameName";
 
 
     @Test
@@ -59,15 +63,19 @@ public class Tests extends BaseTest{
                 .body("name", is(not(equalTo(null))));
     }
 
-    //@Test
-    //public void PostSameName_Test(){
+    @Test
+    public void PostSameName_Test(){
 
-      //  request
-        //        .post (String.format("%s", ResourcePostSameName))
-          //      .then()
-            //    .statusCode(200)
-              //  .header("Access-Control-Allow-Origin", equalTo("http://localhost"))
-               // .body("message", equalTo("Please login first"));
-    //}
+        JSONObject PostSameName = new JSONObject();
+        PostSameName.put("name", "Undertaker");
+
+        request
+               .body(PostSameName.toJSONString())
+                .post(ResourcePostSameName)
+                .then()
+                .statusCode(200)
+                .header("Access-Control-Allow-Origin", equalTo("http://localhost"))
+                .body("name", equalTo("Undertaker"));
+    }
 
 }
